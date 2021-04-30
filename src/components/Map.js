772580c517eae5917fb6d534993b5762781d0b8c
Meredith Cornelius <meredith.cornelius@gmail.com>
2021-04-30 13:22:54 -0400

    feat: can now find user location and set state with coords, added a margin around the map for better user experience

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,69 +2,12 @@ import React, { Component } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 export default class Map extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lat: 40.70341248101454,
-      lng: -73.92393539467943,
-      zoom: 13,
-    };
-  }
-
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const newCoords = JSON.stringify({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        });
-        console.log(newCoords);
-      },
-      (error) => {
-        console.log(error);
-      },
-      { timeout: 10000, maximumAge: 60000 }
-    );
-    if ("geolocation" in navigator) {
-      console.log("geolocation is available");
-    } else {
-      console.log("geolocation is not available");
-    }
-
-    if (navigator.geolocation) {
-      navigator.permissions
-        .query({ name: "geolocation" })
-        .then(function (result) {
-          if (result.state === "granted") {
-            console.log("permission is granted");
-          } else if (result.state === "prompt") {
-            console.log(result.state);
-          } else if (result.state === "denied") {
-            console.log("you need to enable your location!");
-          }
-        });
-    } else {
-      alert("sorry not available");
-    }
-  }
-
-  //   success(position) {
-  //     console.log(position);
-  //     const latitude = position.coords.latitude;
-  //     const longitude = position.coords.longitude;
-  //     console.log("this is latitude", latitude);
-  //     this.setState({ lat: latitude, lng: longitude });
-  //   }
-
-  //   noLocation() {
-  //     console.log("NO LOCATION");
-  //   }
-
   render() {
+    console.log("this is props:", this.props);
     return this.props.dogRuns ? (
       <MapContainer
-        center={[this.state.lat, this.state.lng]}
-        zoom={this.state.zoom}
+        center={[this.props.lat, this.props.lng]}
+        zoom={this.props.zoom}
         scrollWheelZoom={true}
       >
         <TileLayer
