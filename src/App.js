c396a43125b66ca4de973d369843f413a2a4c3eb
Meredith Cornelius <meredith.cornelius@gmail.com>
2021-05-01 17:20:02 -0400

    added redux

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ class App extends Component {
       <div className="main-app-div">
         <div className="header">
           <h2>DogGo NYC</h2>
-          <p>
+          <p className="website-desc">
             A mobile website that helps dog owners and walkers find the nearest
             dog run when out and about with a pup in the Big Apple.
           </p>
